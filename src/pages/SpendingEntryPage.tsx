32b6e345b51abb0c46382da81f52f0b736cefedc
Mavericks Umbrella <mavericks.u.research@gmail.com>
2025-07-13 14:57:53 -0400

    Added transation history to Spending Entry Page

--- src/pages/SpendingEntryPage.tsx
+++ src/pages/SpendingEntryPage.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import SpendingForm from '../components/forms/SpendingForm';
+import TransactionList from "../components/Dashboard/RecentTxList";
 
 const SpendingEntryPage: React.FC = () => {
   return (
     <div>
       <h1>Enter a New Expense</h1>
       <SpendingForm />
+      <TransactionList />
     </div>
   );
 };
